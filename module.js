a2d873d1d23301bed25f28428923ae4aff6f5e51
Unknown <skongy530@gmail.com>
2018-01-15 18:25:20 +0800

    0115

--- module.js
+++ module.js
@@ -8,9 +8,6 @@
 		this.options = options;
 	};
 
-
-
-
 	Module.DEFAULTS = {
 		beginShow: false,
 		class: {
@@ -46,7 +43,21 @@
 		var options = this.options
 		var start = this.options.position.start
 		var end = this.options.position.end
-		this.$nvb_gptb.append('<div class="' + options.class.main + '" style=top:' + options.position.top +'><ul class="' + options.class.ul.li_list + '"><li><a  >' + options.anchors[1].name + '</a></li><li><a  >' + options.anchors[2].name + '</a></li><li><a  >' + options.anchors[3].name + '</a></li><li><a  >' + options.anchors[4].name + '</a></li><li><a  >' + options.anchors[5].name + '</a></li><li><a  >' + options.anchors[6].name + '</a></li><li><a  >' + options.anchors[7].name + '</a></li><li><a  >' + options.anchors[8].name + '</a></li></ul></div><div class=content><div class=sec1>'+ options.anchors[1].name +'</div><div class=sec2>'+ options.anchors[2].name +'</div><div class=sec3>'+ options.anchors[3].name +'</div><div class=sec4>'+ options.anchors[4].name +'</div><div class=sec5>'+ options.anchors[5].name +'</div><div class=sec6>'+ options.anchors[6].name +'</div><div class=sec7>'+ options.anchors[7].name +'</div><div class=sec8>'+ options.anchors[8].name +'</div></div>');
+		var htmlStr = "";
+		// this.$nvb_gptb.append('<div class="' + options.class.main + '" style=top:' + options.position.top +'><ul class="' + options.class.ul.li_list + '">');
+		// for(var i = 1; i < options.anchors.length; i++) {
+		// 	this.$nvb_gptb.append('<li><a  >' + options.anchors[i].name + '</a></li>');
+		// }
+		// this.$nvb_gptb.append('</ul></div><div class=content><div class=sec1>'+ options.anchors[1].name +'</div><div class=sec2>'+ options.anchors[2].name +'</div><div class=sec3>'+ options.anchors[3].name +'</div><div class=sec4>'+ options.anchors[4].name +'</div><div class=sec5>'+ options.anchors[5].name +'</div><div class=sec6>'+ options.anchors[6].name +'</div><div class=sec7>'+ options.anchors[7].name +'</div><div class=sec8>'+ options.anchors[8].name +'</div></div>');
+
+		htmlStr += '<div class="' + options.class.main + '" style=top:' + options.position.top +'><ul class="' + options.class.ul.li_list + '">';
+		for(var i = 1; i < options.anchors.length; i++) {
+			htmlStr += '<li><a  >' + options.anchors[i].name + '</a></li>';
+		}
+		htmlStr += '</ul></div><div class=content><div class=sec1>'+ options.anchors[1].name +'</div><div class=sec2>'+ options.anchors[2].name +'</div><div class=sec3>'+ options.anchors[3].name +'</div><div class=sec4>'+ options.anchors[4].name +'</div><div class=sec5>'+ options.anchors[5].name +'</div><div class=sec6>'+ options.anchors[6].name +'</div><div class=sec7>'+ options.anchors[7].name +'</div><div class=sec8>'+ options.anchors[8].name +'</div></div>';
+		
+		this.$nvb_gptb.append(htmlStr);
+
 		$('li').click(function(){
 			$(this).addClass('active').siblings().removeClass('active');
 		});
