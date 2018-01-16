e9bdaa0b7858e0e767f5533ee83490aba0a9b3bc
Unknown <skongy530@gmail.com>
2018-01-16 08:47:02 +0800

    0116

--- module.js
+++ module.js
@@ -10,87 +10,137 @@
 
 	Module.DEFAULTS = {
 		beginShow: false,
-		class: {
-			main: 'nav-container',
-			ul: {
-				li_list: 'li-list container',
-				li: 'li'
-			}
-		},
 		anchors: [
 		$('.dom'),
 		{
 			name: '行程特色', 
-			moveto: 0 || '0px' || $DOM,
+			moveto: 400 || '400px' || $DOM,
 		},
 		],
-		dropOffset: 0 || '0px',
+		dropOffset: 60 || '60px',
 		fixedClass: 'fixed',
 		position: {
-			start: 0 || '0px',
-			end: 0 || '0px' || false,
+			start: 200 || '200px',
+			end: 2000 || '2000px' || false,
 			top: 0 || '0px'
 		}
 	};
 
 
 	Module.prototype.init = function(){
-		this.create();
-	};
+		var options = this.options ;
+		var start = this.options.position.start ;
+		var end = this.options.position.end ;
+		var dropOffset = this.options.dropOffset ;
+		var htmlStr = "" ;
 
-
-	Module.prototype.create = function(){
-		var options = this.options
-		var start = this.options.position.start
-		var end = this.options.position.end
-		var htmlStr = "";
-		// this.$nvb_gptb.append('<div class="' + options.class.main + '" style=top:' + options.position.top +'><ul class="' + options.class.ul.li_list + '">');
-		// for(var i = 1; i < options.anchors.length; i++) {
-		// 	this.$nvb_gptb.append('<li><a  >' + options.anchors[i].name + '</a></li>');
-		// }
-		// this.$nvb_gptb.append('</ul></div><div class=content><div class=sec1>'+ options.anchors[1].name +'</div><div class=sec2>'+ options.anchors[2].name +'</div><div class=sec3>'+ options.anchors[3].name +'</div><div class=sec4>'+ options.anchors[4].name +'</div><div class=sec5>'+ options.anchors[5].name +'</div><div class=sec6>'+ options.anchors[6].name +'</div><div class=sec7>'+ options.anchors[7].name +'</div><div class=sec8>'+ options.anchors[8].name +'</div></div>');
-
-		htmlStr += '<div class="' + options.class.main + '" style=top:' + options.position.top +'><ul class="' + options.class.ul.li_list + '">';
-		for(var i = 1; i < options.anchors.length; i++) {
-			htmlStr += '<li><a  >' + options.anchors[i].name + '</a></li>';
+		htmlStr += '<div class=nav-container style=top:' + options.position.top +'><ul class="li-list container">';
+		for(var i = 0; i < options.anchors.length; i++) {
+			if( ( typeof options.anchors[i].name  === 'string') ) 
+			htmlStr += '<li class=li'+[i]+'><a>' + options.anchors[i].name + '</a></li>';
+		};
+		htmlStr += '</ul></div><div class=content>';
+		for( var i = 0; i < 8; i++){
+			if( ( typeof options.anchors[i].name  === 'string') ) 
+			htmlStr += '<div class=sec'+ [i] +'>行程特色'+ [i] +'</div>';
 		}
-		htmlStr += '</ul></div><div class=content><div class=sec1>'+ options.anchors[1].name +'</div><div class=sec2>'+ options.anchors[2].name +'</div><div class=sec3>'+ options.anchors[3].name +'</div><div class=sec4>'+ options.anchors[4].name +'</div><div class=sec5>'+ options.anchors[5].name +'</div><div class=sec6>'+ options.anchors[6].name +'</div><div class=sec7>'+ options.anchors[7].name +'</div><div class=sec8>'+ options.anchors[8].name +'</div></div>';
-		
+		htmlStr += '</div>';
 		this.$nvb_gptb.append(htmlStr);
 
-		$('li').click(function(){
-			$(this).addClass('active').siblings().removeClass('active');
-		});
+
+		for(var i = 0; i < options.anchors.length; i++) {
+			$('.li' + [i] ).on('click',function(){
+				var $this = $(this).index();
+				var moveto = options.anchors[$this].moveto ;
+				$('html, body').animate({
+					scrollTop: (moveto-dropOffset)
+				}, 500);
+				$(window).scroll(function(){
+				var scrollVal = $(this).scrollTop();
+				if( scrollVal > 200 ){
+					$('.li' + $this ).addClass('active').siblings().removeClass('active');
+				}else{
+					console.log(scrollVal)
+					console.log(moveto)
+				}
+				})
+			})
+		};
+
+		// for(var i = 0; i < options.anchors.length; i++) {
+		// 	$(window).scroll(function(){	
+		// 	var moveto = options.anchors[i].moveto ;
+	 //     	var scrollVal = $(this).scrollTop();
+	 //     	console.log(moveto)
+		// 	if( scrollVal > moveto ){
+		// 		$('.li' + [i] ).addClass('active');
+		// 	}else{
+		// 		console.log(1);
+		// 	}
+		// });
+		// };
+
+
+
 		if( this.options.beginShow ) {
-			$('.' + options.class.main ).addClass('d-block');
+			$('.nav-container').addClass('d-block');
+			$(window).scroll(function () {
+			    var scrollVal = $(this).scrollTop();
+			    if( scrollVal > start ){
+			    	$('.nav-container').addClass( options.fixedClass ).addClass('d-block');
+			    } else {
+			    	$('.nav-container').removeClass( options.fixedClass ).removeClass('d-block');			    	
+			    }
+			  });
 		} else {
 			  $(window).scroll(function () {
 			    var scrollVal = $(this).scrollTop();
 			    if( scrollVal > start ){
-			    	$('.' + options.class.main ).addClass('fixed').addClass('d-block');
+			    	$('.nav-container').addClass( options.fixedClass ).addClass('d-block');
 			    } else {
-			    	$('.' + options.class.main ).removeClass('fixed').removeClass('d-block');			    	
+			    	$('.nav-container').removeClass( options.fixedClass ).removeClass('d-block');			    	
 			    }
 			  });
 		}
+		// this.addBottomLine();
 	};
 
 
-	$.fn[ModuleName] = function ( method, options ) {
+	// Module.prototype.addBottomLine = function(){
+	// 	var options = this.options ;
+
+	// 	for(var i = 0; i < options.anchors.length; i++) {
+	// 		$(window).scroll(function(){
+	// 			console.log($(this).index())
+	// 		var $this = $(this).index();	
+	// 		var moveto = options.anchors[$this].moveto ;
+	//      	var scrollVal = $(this).scrollTop();
+	// 		if( scrollVal < moveto ){
+	// 			console.log(0)
+	// 			$('.li' + [i] ).addClass('active');
+	// 		}else{
+	// 			console.log(1);
+	// 		}
+	// 	});
+	// 	};
+	// };
+
+
+
+
+	$.fn[ModuleName] = function ( options, options2 ) {
 		return this.each(function(){
 			var $this = $(this);
 			var module = $this.data( ModuleName );
 			var opts = null;
 			if ( !!module ) {
-				if ( typeof method === 'string' &&  typeof options === 'undefined' ) {
-					module[method]();
-				} else if ( typeof method === 'string' && typeof options === 'object' || typeof options === 'string' ) {
-					module[method](options);
+				if ( typeof options === 'string' &&  typeof options2 === 'undefined' ) {
+					module[options]();
 				} else {
 					console.log('unsupported options!');
 				}
 			} else {
-				opts = $.extend( {}, Module.DEFAULTS, ( typeof method === 'object' && method ), ( typeof options === 'object' && options ) );
+				opts = $.extend( {}, Module.DEFAULTS, ( typeof options === 'object' && options ), ( typeof options2 === 'object' && options2 ) );
 				module = new Module(this, opts);
 				$this.data( ModuleName, module );
 				module.init();
